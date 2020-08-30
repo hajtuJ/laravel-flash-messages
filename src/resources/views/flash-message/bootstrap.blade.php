880e9216f24c31de8a50e2796b505d2269adcac1
Sebastian Bolek <bolek.sebastian@gmail.com>
2020-08-30 20:46:40 +0000

    Used class via Alias

--- src/resources/views/flash-message/bootstrap.blade.php
+++ src/resources/views/flash-message/bootstrap.blade.php
@@ -1,7 +1,7 @@
-@if(\FlashMessages\FlashMessage\Facades\FlashMessage::hasMessage())
+@if(FlashMessage::hasMessage())
 
-    <div class="alert {{ \FlashMessages\FlashMessage\Facades\FlashMessage::getMessage()['class'] }} alert-dismissible fade show" role="alert">
-        {{  \FlashMessages\FlashMessage\Facades\FlashMessage::getMessage()['text'] }}
+    <div class="alert {{ FlashMessage::getMessage()['class'] }} alert-dismissible fade show" role="alert">
+        {{  FlashMessage::getMessage()['text'] }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
